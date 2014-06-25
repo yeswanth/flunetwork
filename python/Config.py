53ab0011f7b3b8e4b3ce65cdba032f048d056a61
Akash <akash.waran@gmail.com>
2014-06-25 03:40:40 -0400

    Basic network model

---
+++ python/Config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Config(object):
+
+    def __init__(self):
+	self.TOTAL_NODES = 1000
+
--- python/Network.py
+++ python/Network.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from Config import Config
 from Node import Node
 
 class Network(object):
 
     def __init__(self):
+	self.config = Config()
+	self.graph = {}
 	self._initializeNetwork()
 
     def _initializeNetwork(self):
 	print "Initializing network"
+	current_node=0
+	while current_node < self.config.TOTAL_NODES:
+	    self.graph[current_node] = Node()
+	    current_node += 1
+	print "Finished initializing network"
 
