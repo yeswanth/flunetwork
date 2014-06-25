0e8a4cdc5a5dcd281b4d594641a02982d78a3ce0
Akash <akash.waran@gmail.com>
2014-06-25 03:57:54 -0400

    Modifying config file to better reflect configuration

--- python/Config.py
+++ python/Config.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class Config(object):
-
-    def __init__(self):
-	self.TOTAL_NODES = 1000
+TOTAL_NODES = 1000
 
--- python/Network.py
+++ python/Network.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Config import Config
+from Config import *
 from Node import Node
 
 class Network(object):
 
     def __init__(self):
-	self.config = Config()
 	self.graph = {}
 	self._initializeNetwork()
 
     def _initializeNetwork(self):
 	print "Initializing network"
 	current_node=0
-	while current_node < self.config.TOTAL_NODES:
+	while current_node < TOTAL_NODES:
 	    self.graph[current_node] = Node()
 	    current_node += 1
 	print "Finished initializing network"
