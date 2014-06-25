1ec42a2d24a8a0d9f9fea268879877f73bf2df9e
Akash <akash.waran@gmail.com>
2014-06-25 03:19:15 -0400

    Initial stubs

---
+++ python/Network.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Node import Node
+
+class Network(object):
+
+    def __init__(self):
+	self._initializeNetwork()
+
+    def _initializeNetwork(self):
+	print "Initializing network"
+
---
+++ python/Node.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Node(object):
+
+    def __init__(self):
+	print "Initializing node"
+
